9f15ecdb9162f60e2a850976a584deb4ceaf413b
amalfi <mberendt546@gmail.com>
2016-04-16 20:10:03 +0200

    Initial commit

---
+++ cassandra_java_client/src/main/java/com/cassandra/client/Main.java
@@ -0,0 +1,27 @@
+package com.cassandra.client;
+
+import java.util.Iterator;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CassandraClient cassandraClient = new CassandraClient();
+		cassandraClient.connect("127.0.0.1");
+		ResultSet resultSet = (ResultSet) cassandraClient.executeQuery("select * from emp;", "hr");
+		printResultToConsole(resultSet);
+		cassandraClient.close();
+	}
+
+	private static void printResultToConsole(ResultSet result) {
+		Iterator<Row> resultIterator = result.iterator();
+		while(resultIterator.hasNext())
+		{
+			Row currentRow = resultIterator.next();
+			System.out.println(currentRow);
+		}
+	}
+
+}
